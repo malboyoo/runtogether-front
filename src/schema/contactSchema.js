3ff20077ae061700d709b6c43e1e7f7445b13d0d
Ubuntu <ubuntu@vps-ea909e1b.vps.ovh.net>
2023-01-02 15:28:09 +0100

    fixing schema

--- src/schema/contactSchema.js
+++ src/schema/contactSchema.js
@@ -3,8 +3,8 @@ import * as yup from "yup";
 export const contactSchema = yup.object({
   name: yup
     .string()
-    .required("Il faut préciser un nom pour votre sortie")
-    .min(4, "nom trop court")
+    .required("Il faut préciser votre nom")
+    .min(3, "nom trop court")
     .max(70, "nom trop long"),
   email: yup.string().email().required("l'e-mail est obligatoire"),
   content: yup.string().required("veuillez précisez le contenu du message").min(2, "message trop court"),
